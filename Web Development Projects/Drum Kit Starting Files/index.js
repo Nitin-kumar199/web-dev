02b48df0990c02e747f9083b14899c3fa9c493d7
Nitin Kumar <199nitinkumar@gmail.com>
2023-08-20 19:41:44 +0530

    jQuery learning

--- Web Development Projects/Drum Kit Starting Files/index.js
+++ Web Development Projects/Drum Kit Starting Files/index.js
@@ -2,37 +2,54 @@ var numberOfButton = document.querySelectorAll(".drum").length;
 for (var i = 0; i < numberOfButton; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var btninnerHtml = this.innerHTML;
-    switch (btninnerHtml) {
-      case "w":
-        var tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
-      case "a":
-        var tom2 = new Audio("sounds/tom-2.mp3");
-        tom2.play();
-        break;
-      case "s":
-        var tom3 = new Audio("sounds/tom-3.mp3");
-        tom3.play();
-        break;
-      case "d":
-        var tom4 = new Audio("sounds/tom-4.mp3");
-        tom4.play();
-        break;
-      case "j":
-        var crash = new Audio("sounds/crash.mp3");
-        crash.play();
-        break;
-      case "k":
-        var kick = new Audio("sounds/kick-bass.mp3");
-        kick.play();
-        break;
-      case "l":
-        var snare = new Audio("sounds/snare.mp3");
-        snare.play();
-        break;
-      default:
-        console.log(btninnerHtml);
-    }
+    makeSound(btninnerHtml);
+    addAnimation(btninnerHtml);
   });
+}
+
+document.addEventListener("keypress", function (event) {
+  makeSound(event.key);
+  addAnimation(event.key);
+  console.log(event.key);
+});
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "k":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    case "l":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    default:
+      console.log(key);
+  }
+}
+function addAnimation(currentKey) {
+  var btn = document.querySelector("." + currentKey).classList;
+  btn.add("pressed");
+  setTimeout(function () {
+    btn.remove("pressed");
+  }, 500);
 }
---
+++ jQuery/index.js
@@ -0,0 +1,8 @@
+$(document).keypress(function (event) {
+  console.log(event.key);
+  $("h1").text(event.key);
+});
+
+$("h1").on("mouseover", function () {
+  $("h1").css("color", "red");
+});
